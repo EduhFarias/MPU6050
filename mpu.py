d95577642692ff4cbd800221f24e2e4548891265
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2022-06-10 13:42:46 -0300

    Update mpu.py

--- mpu.py
+++ mpu.py
@@ -1,7 +1,20 @@
 from mpu6050 import mpu6050
+import numpy as np
+import json
 
 sensor = mpu6050(0x68)
 
-a_x, a_y, a_z = sensor.get_accel_data()
+x, y, z = [], [], []
+i = 0
 
-print('X: {} | Y: {} | Z: {}'.format(a_x, a_y, a_z))
+
+  with open(filename, 'wb') as outp:  # Overwrites any existing file.
+  pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+
+while(i < 1):
+  a_x, a_y, a_z = sensor.get_accel_data()
+  print('X: {} | Y: {} | Z: {}'.format(a_x, a_y, a_z))
+  i += 0.01
+
+with open(tst1.json, 'w') as outp:  # Overwrites any existing file.
+  json.dump({'x': x, 'y': y, 'z': z}, outp)
