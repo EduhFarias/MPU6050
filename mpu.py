9b684fc9165eda06d20a52c91df0aa5fb33e8f5a
Eduardo Henrique <ehfs@ic.ufal.br>
2022-08-14 20:01:33 +0000

    mpu

--- mpu.py
+++ mpu.py
@@ -5,18 +5,25 @@ from time import sleep
 
 sensor = mpu6050(0x68)
 
-x, y, z = [], [], []
+xa, ya, za = [], [], []
+xg, yg, zg = [], [], []
 i = 0
 
-while(i < 50):
+while(i < 20):
   accel_data = sensor.get_accel_data()
+	gyro = sensor.get_gyro_data()
   ax, ay, az = accel_data['x'], accel_data['y'], accel_data['z']
-  print('X: {} | Y: {} | Z: {}'.format(ax, ay, az))
-  x.append(ax)
-  y.append(ay)
-  z.append(az)
-  i += 0.05
-  sleep(0.05)
+  gx. gy, gz = gyro['x'], gyro['z'], gyro['z']
+	print('Accel -> X: {} | Y: {} | Z: {}'.format(ax, ay, az))
+	print('Gyro  -> X: {} | Y: {} | Z: {}'.format(gx, gy, gz))
+  xa.append(ax)
+  ya.append(ay)
+  za.append(az)
+	xg.append(gx)
+	yg.append(gy)
+	zg.append(gz)
+  i += 0.5
+  sleep(0.5)
 
-with open('tst1.json', 'w') as outp:  # Overwrites any existing file.
-  json.dump({'x': x, 'y': y, 'z': z}, outp)
+with open('mpu.json', 'w') as outp:  # Overwrites any existing file.
+  json.dump({'accel': {'x': xa, 'y': ya, 'z': za}, 'gyro': {'x':xg, 'y':yg, 'z':zg}}, outp)
